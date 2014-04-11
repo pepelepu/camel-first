1a9b016e2613c29e6cca54fbc91dd27c6abd2914
pepelepu <andres.denastchokine@snoopconsulting.com>
2014-04-11 11:23:51 -0300

    sssss

--- camel-first/src/main/java/com/camel/first/route/CaseUnoRoute.java
+++ camel-first/src/main/java/com/camel/first/route/CaseUnoRoute.java
@@ -23,7 +23,7 @@ public class CaseUnoRoute extends RouteBuilder {
 										// en el xml
 
 				// lamada al WS
-				.to("spring-ws:http://baslije12.nextel.com.ar:7778/avalonWS/AvalonWS")
+				.to("spring-ws:http://xxxxxxxxxxxx.com.ar:7778/avalonWS/AvalonWS")
 
 				// log del body
 				.log(LoggingLevel.INFO, "${body}");
