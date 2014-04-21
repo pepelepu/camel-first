a2cbe12d5efe074fc71149091475f680f4df48cc
pepelepu <andres.denastchokine@snoopconsulting.com>
2014-04-21 12:27:33 -0300

    fefefefef

--- camel-first/src/test/java/com/camel/first/MyFirstTest.java
+++ camel-first/src/test/java/com/camel/first/MyFirstTest.java
@@ -1,5 +1,9 @@
 package com.camel.first;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractXmlApplicationContext;
@@ -7,13 +11,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyFirstTest extends CamelSpringTestSupport {
 
+	@EndpointInject(uri = "mock:result")
+	protected MockEndpoint resultEndpoint;
+
+	@Produce(uri = "direct:start")
+	protected ProducerTemplate template;
+
 	@Override
 	protected AbstractXmlApplicationContext createApplicationContext() {
 		return new ClassPathXmlApplicationContext("camel-context-mock.xml");
 	}
 
 	@Test
-	public void testFirst() {
-		template.sendBody("codeFirstEndpoint", "aaaaaaaaaaaaaaa");
+	public void testFirst() throws Exception {
+
+		String expectedBody = "<obtenerContratoCuentaReturn><cantActivos>0</cantActivos><cantNoActivos>6</cantNoActivos><cantSuspendidos>0</cantSuspendidos><customerCode>2.13</customerCode></obtenerContratoCuentaReturn>";
+
+		resultEndpoint.expectedBodiesReceived(expectedBody);
+
+		template.sendBody(expectedBody);
+		// template.sendBodyAndHeader(expectedBody, "foo", "bar");
+
+		resultEndpoint.assertIsSatisfied();
 	}
 }
